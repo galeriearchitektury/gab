611472651695155f1a1c367a4f9313ab7104e343
erikvanek <vanek.erik@gmail.com>
2023-02-25 10:56:23 +0100

    tuning gallery

--- pages/js/eurotopia-gallery.mjs
+++ pages/js/eurotopia-gallery.mjs
@@ -1,6 +1,7 @@
 // https://medium.com/axlewebtech/upload-a-file-in-github-using-github-apis-dbb6f38cc63
 
 let knownImages = [];
+let zIndexCounter = 0;
 
 const initGallery = async () => {
     const contents = await fetch(
@@ -18,17 +19,30 @@ const initGallery = async () => {
         }
     );
     const json = await contents.json();
-    const fetchedImages = json.map((file) => file.download_url);
+    const fetchedImages = json
+        // .reverse()
+        .map((file) => file.download_url);
 
     const wrapper = document.getElementById('gallery-wrapper');
     fetchedImages
         .filter((img) => !knownImages.includes(img))
-        .forEach((url) => {
+        .forEach((url, index) => {
             const firstChild = wrapper.firstChild;
+            zIndexCounter++;
             knownImages.unshift(url);
+            const pic = document.createElement('div');
+            pic.className = 'pic';
+            const rotateRadom = Math.floor(Math.random() * 40) - 20;
+            const lRandom = Number.parseInt(Math.random() * 100, 10);
+            const rRandom = Number.parseInt(Math.random() * 100, 10);
+            pic.style.left = `calc(${lRandom}% - 30vmin)`;
+            pic.style.top = `calc(${rRandom}% - 30vmin)`;
+            pic.style.transform = `rotateZ(${rotateRadom}deg)`;
+            pic.style.zIndex = zIndexCounter;
             const image = document.createElement('img');
             image.src = url;
-            wrapper.insertBefore(image, firstChild);
+            pic.appendChild(image);
+            wrapper.insertBefore(pic, firstChild);
         });
 };
 
