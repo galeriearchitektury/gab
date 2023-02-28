985cd94e2265101f0b4288d4a910c900009b2ebe
erikvanek <vanek.erik@gmail.com>
2023-02-28 11:01:58 +0100

    add open call gallery and some backdrops

---
+++ docs/js/open-call-gallerie.js
@@ -0,0 +1,141 @@
+const limity = {
+    collective: 'Limity jsme my',
+    text: `
+1. Unie, jejíž členové platí mimounijnímu agresorovi za fosilní paliva 4x více, než uvolňují na obranu napadeného kandidáta na členství, nenaplňuje představy zkoordinovaného společenství a autonomního a bezpečného prostoru.
+
+2. Unii, která za posledních 10 let investuje více než 100 mld CZK na nové projekty plynové infrastruktury již teď vypouštějící více emisí CO2 než hnědé uhlí, lze těžko vnímat jako instituci zodpovědně konající v zájmu jejích obyvatel.
+
+3. Unie, která v rámci fondů usnadňujících odklon od fosilních paliv nedisponuje nástroji proti jejich využití fosilními miliardáři, a která z extrémních mimořádných zisků energetických gigantů zajistí zabavení pouhých jejich 5 %, se výrazně nepřibližuje obrazu tvrdého hráče a dobrého hospodáře.
+
+4. Unie, jejíž stoupenci obhajují konání nadcházejícího klimatického summitu na půdě CEOs ropných megakorporací, stěží působí jako zdatný lídr a vyjednavač.
+
+5. U unie, která představí plán dosažení uhlíkové neutrality míjející se s nezbytným udržením hranice oteplování pod 1.5°C, lze těžko doufat v naplnění deklarované snahy jít celému světu příkladem.
+`,
+    photos: [
+        '../img/eurotopia/backdrops/ljm1-medium.png',
+        '../img/eurotopia/backdrops/ljm2-medium.png',
+        '../img/eurotopia/backdrops/ljm3-medium.png',
+    ],
+    author: [
+        'Majda Slámová',
+        'Petr Racek',
+        'Gabriela Procházka',
+        'Martin Bartoš',
+        'Anna Bromová',
+    ],
+};
+
+const kuba = {
+    photos: [
+        '../img/eurotopia/backdrops/merkel1-medium.png',
+        '../img/eurotopia/backdrops/merkel2-medium.png',
+    ],
+    author: ['Jakub Polách'],
+};
+
+const iran = {
+    text: `
+    Present.
+The flame still burns. And with him police cars and the hope that this time there will
+be a change. I am deeply saddened, there is an escalation and the Western media
+quite often seem to find only sand dunes, terrorists and „endless war“ on the map,
+while somehow forgetting about ordinary people. And so, this time, Iran’s name is
+again connected with the supply of drones for Russia to Ukraine. As if the
+government and the people are one, so at this moment we will sweep the support of
+anti-government demonstrations under the
+„Iran, that bad Iran…“
+
+I am in daily contact with my friends in Iran and every day it is a question of which of
+them I wrote to last.
+
+And so let the hair that traveled across part of the world become my business card
+of defiance and support.
+
+The nooses will always hang, only our level of defiance can delay us so that one
+day the rope is not for us.
+
+
+
+از ایرانی‌ها تشکر می‌کنم، چون به لطف آنهاست که هنری می‌سازم که نظر داشته باشد و هرگز ساکت نشود.
+    `,
+    photos: [
+        '../img/eurotopia/backdrops/iran-medium.png',
+        '../img/eurotopia/backdrops/iran2-medium.png',
+    ],
+    author: ['Dominik Dragos Pohludka'],
+};
+
+const mda = {
+    text: `
+    Digitální revoluce spojená s radikálním nástupem umělé inteligence přináší mnoho otázek jak pro každodenní životy, tak pro celou společnost. Změní se představa, co myslíme pojmem práce? Jak zajistit financování sociálního státu? A jak ovlivní digitální revoluce demokracii? 
+
+    Základním cílem našeho konání ale musí být udržitelná budoucnost. Jen společně a na mezinárodní úrovni můžeme zvládnout klimatickou krizi. Jak ale dosáhnout účinného řešení? Především ale – jak ho dosáhnout, aby si ho všichni vzali za své? 
+    
+    Aby bylo řešení úkolů úspěšné, nesmí na jeho konci zůstat vítězové a poražení. Přitom otázku po spravedlnosti si už dnes můžeme klást každý den: Například jak je možné, že v rámci Evropské unie dostávají dva lidé za stejnou práci zásadně odlišnou odměnu? Jak je možné, že bohaté státy fungují kvůli levné pracovní síle svých sousedů? 
+    
+    Zvládnutí zásadních úkolů dnešní doby pomocí nástrojů liberální demokracie je cílem think-tanku Masarykovy demokratické akademie. 
+    `,
+    photos: ['../img/eurotopia/backdrops/mda-medium.png'],
+    author: ['Masarykova demokratická akademie, z. s.'],
+    collective: ['Masarykova demokratická'],
+};
+
+const hottova = {
+    text: `
+    V rámci výtvarného vyjadřování se dlouhodobě věnuji tématům, o kterých by se, dle mého názoru, mělo více mluvit. Využívám jasnou symboliku a metafory, někdy vygradované do extrému. To vše má samozřejmě své důvody. Shock value je vedlejším účinkem, záměr je především otevřít diskusi a téma dostat na povrch. Po dlouhém zvažování jsem zvolila ilustraci, která se věnuje aktuální problematice, cloumající se zažitými pořádky v české společnosti. Kult osobnosti je rozbit a z jeho útrob se valí hořká fakta. Avšak, po krátkém pohledu na veřejný diskurz (a to zejména na sociálních sítích) se může zdát, že mezi viníky jsou řazeny všechny zúčastněné strany. Tvrzení, že jde o nebezpečný precedent, překřikují úlevu z faktu, že se taková jednání postupně začínají klasifikovat jako trestný čin. 
+Veškerý výtěžek z ilustrací a obrazů, věnujících se obdobným tématům, je stoprocentně přeposílán organizacím a subjektům, kteří se aktivně věnují osvětě a snaze vzniknuvší škody kompenzovat.
+    `,
+    photos: ['../img/eurotopia/backdrops/hottova-medium.png'],
+    author: ['Ester Hottová'],
+};
+
+const greens = {
+    text: `Pro jedno z pozadí interaktivní instalace jsem zvolila fotografii z Ukrajiny od skvělé české fotografky Majdy Slámové, která od prvních dnů invaze dokumentuje dopady války nejen na architekturu, ale především na tamní obyvatele. I zničená architektura vypovídá o politickém kontextu a ovlivňuje to, jakým způsobem se lidé v jejím okolí cítí a jakým způsobem se vztahují ke své politické reprezentaci.`,
+    photos: [
+        '../img/eurotopia/backdrops/greens-medium.png',
+        '../img/eurotopia/backdrops/greens-2-medium.png',
+    ],
+    author: 'Markéta Gregorová, Majda Slámová',
+    collective: 'Greens/EFA',
+};
+
+const artworks = [limity, kuba, iran, mda, hottova, greens];
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const galleryHolder = document.getElementById('opencall-gallery');
+    artworks.forEach((artwork) => {
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('submission');
+        const heading = document.createElement('h3');
+
+        const text = document.createElement('p');
+        if (artwork.text) {
+            text.innerText = artwork.text;
+        }
+        heading.innerText = artwork.collective ?? artwork.author.join(', ');
+
+        const photos = artwork.photos.map((photo) => {
+            const img = document.createElement('img');
+            img.src = photo;
+            return img;
+        });
+
+        wrapper.appendChild(heading);
+        if (artwork.collective) {
+            const wrp = document.createElement('div');
+            wrp.style.display = 'flex'
+            const h4 = document.createElement('h4');
+            h4.style.marginRight = '.5rem'
+            wrp.style.marginTop = '.5rem'
+            const aut = document.createElement('span');
+            aut.innerText = artwork.author.join(', ');
+            h4.innerText = 'Autorství: ';
+            wrp.appendChild(h4);
+            wrp.appendChild(aut);
+            wrapper.appendChild(wrp);
+        }
+        wrapper.appendChild(text);
+        photos.forEach((photo) => wrapper.appendChild(photo));
+        galleryHolder.appendChild(wrapper);
+    });
+});
